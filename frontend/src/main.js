8232bf11f0a9dac3d41b2fd93a936d240dfaa192
Diogo Matos <diogo.s.matos@tecnico.ulisboa.pt>
2021-04-18 22:45:09 +0100

    Add mysql and set up router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router';
+import router from './plugins/router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 
