cdb1ba91a941dc5c92e6615bd40ff6cc7a6e6a15
ana <anaisabelmartins@tecnico.ulisboa.pt>
2021-04-29 09:27:47 +0100

    fixed cannot get /

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,12 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+  console.log(req.query);
+
+  res.send('Hello World!');
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on http://localhost:${process.env.PORT}`);
 });
