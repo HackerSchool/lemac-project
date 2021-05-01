baf15b15bc9115eb65c5b0c740d974fb7dbbd5cc
Diogo Matos <diogo.s.matos@tecnico.ulisboa.pt>
2021-05-01 12:40:54 +0100

    Add initial colours to frontend

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld/>
+    <HelloWorld />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
