be30e37974cdee3a15821a3fdb69160be9d1fd18
Diogo Matos <diogo.s.matos@tecnico.ulisboa.pt>
2021-05-06 00:05:23 +0100

    Forgot to init the handler middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,16 @@
 require('dotenv').config();
 
 const express = require('express');
-const app = express();
 
 const { dbMiddleware } = require('./middleware/database');
+const { errorHandler } = require('./middleware/requestHandler');
 
 port = process.env.PORT || 5000;
 
+const app = express();
+
 app.use(dbMiddleware);
+app.use(errorHandler);
 
 app.get('/', (req, res) => {
   console.log(req.query);
