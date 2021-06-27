6b10eaab0df84a2ff89e3fbf5de6feb75395b47c
Diogo Matos <diogo.s.matos@tecnico.ulisboa.pt>
2021-06-27 07:29:35 +0100

    Create Homepage with button to trigger login Fixes #10 - login logic

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="about">
-    <h1>This will be the login page</h1>
+    <v-btn @click="login">Login</v-btn>
   </div>
 </template>
+
+<script>
+import apiLogin from '@/api/auth.api';
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    const fenixCode = this.$route.query.code;
+    if (fenixCode) this.authBackend(fenixCode);
+  },
+  methods: {
+    login: () => {
+      window.location = `${process.env.VUE_APP_FENIX_BASE_URL}oauth/userdialog?client_id=${process.env.VUE_APP_FENIX_CLIENT_ID}&redirect_uri=${process.env.VUE_APP_FENIX_REDIRECT_URL}`;
+    },
+    authBackend: async (code) => {
+      try {
+        const { data } = await apiLogin(code);
+        console.log(data);
+        //this.$router.push('/');
+      } catch (e) {
+        console.log(e);
+      }
+    },
+  },
+};
+</script>
