79a2c2d991ee5165161da97630d451a9fc5edf72
Diogo Matos <diogo.s.matos@tecnico.ulisboa.pt>
2021-06-29 22:10:25 +0100

    Header and fully implement login

---
+++ frontend/src/components/Home/HomeHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app-bar app dense>
+    <h2 class="primary--text mx-3">LEMAC</h2>
+    <v-spacer />
+    <v-btn
+      @click="$emit('login')"
+      class="primary mx-3"
+      :loading="loading"
+      :disabled="loading"
+      elevation="2"
+      small
+      >Login</v-btn
+    >
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader',
+  props: ['loading'],
+};
+</script>
+
+<style></style>
--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,17 +1,49 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld />
+    <HomeHeader @login="login" :loading="loading" />
   </div>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue';
-
+import HomeHeader from '@/components/Home/HomeHeader.vue';
+import apiLogin from '@/api/auth.api';
+import { mapActions } from 'vuex';
 export default {
   name: 'Home',
+  data: () => ({
+    loading: false,
+  }),
   components: {
-    HelloWorld,
+    HomeHeader,
+  },
+  mounted() {
+    const fenixCode = this.$route.query.code;
+    if (fenixCode) this.authBackend(fenixCode);
+  },
+  methods: {
+    login: () => {
+      window.location = `${process.env.VUE_APP_FENIX_BASE_URL}oauth/userdialog?client_id=${process.env.VUE_APP_FENIX_CLIENT_ID}&redirect_uri=${process.env.VUE_APP_FENIX_REDIRECT_URL}`;
+    },
+    authBackend: async function (code) {
+      this.loading = true;
+      try {
+        const { data } = await apiLogin(code);
+        if (data.jwt) {
+          localStorage.setItem('token', data.jwt);
+          this.loginUser(data.user);
+        }
+      } catch (e) {
+        this.$notify({
+          type: 'error',
+          title: 'Unauthorized user',
+          text: "You don't have permission to access this ",
+          duration: -1,
+        });
+      }
+      this.loading = false;
+      this.$router.push('/');
+    },
+    ...mapActions('user', ['loginUser', 'logoutUser']),
   },
 };
 </script>
