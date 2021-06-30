f93de7ee4e9a7cca769e9bd70cb7ac819d881e28
Diogo Matos <diogo.s.matos@tecnico.ulisboa.pt>
2021-06-30 13:39:20 +0100

    Logout finished

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <HomeHeader @login="login" :loading="loading" />
+    <HomeHeader @login="login" @logout="logout" :loading="loading" :loadingOut="loadingOut" />
   </div>
 </template>
 
@@ -12,6 +12,7 @@ export default {
   name: 'Home',
   data: () => ({
     loading: false,
+    loadingOut: false,
   }),
   components: {
     HomeHeader,
@@ -19,12 +20,26 @@ export default {
   mounted() {
     const fenixCode = this.$route.query.code;
     if (fenixCode) this.authBackend(fenixCode);
+
+    const jwt = localStorage.getItem('token');
+    if (jwt) {
+      const res = parseJwt(jwt);
+      const user = {
+        id: res.id,
+        istId: res.istId,
+        name: res.name,
+        active: res.active,
+        admin: res.admin,
+      };
+      this.loginUser(user);
+    }
   },
   methods: {
-    login: () => {
+    login() {
       window.location = `${process.env.VUE_APP_FENIX_BASE_URL}oauth/userdialog?client_id=${process.env.VUE_APP_FENIX_CLIENT_ID}&redirect_uri=${process.env.VUE_APP_FENIX_REDIRECT_URL}`;
     },
-    authBackend: async function (code) {
+    //only runs after fenix login
+    async authBackend(code) {
       this.loading = true;
       try {
         const { data } = await apiLogin(code);
@@ -43,7 +58,29 @@ export default {
       this.loading = false;
       this.$router.push('/');
     },
+    logout() {
+      this.loadingOut = true;
+      localStorage.removeItem('token');
+      this.logoutUser();
+      this.loadingOut = false;
+    },
     ...mapActions('user', ['loginUser', 'logoutUser']),
   },
 };
+
+//gets the user data from the middle section of the jwt if its already present in local storage
+function parseJwt(token) {
+  var base64Url = token.split('.')[1];
+  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split('')
+      .map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join('')
+  );
+
+  return JSON.parse(jsonPayload);
+}
 </script>
