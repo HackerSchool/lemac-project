1cc9848fe16587dfb86cc94b1e470d067f33ccf1
Diogo Matos <diogo.s.matos@tecnico.ulisboa.pt>
2021-08-04 19:27:43 +0100

    finish token auth middleware (closes #14)

--- backend/src/middleware/verifier.js
+++ backend/src/middleware/verifier.js
@@ -1,13 +1,21 @@
-// não sei se isto está bem aqui ou se é melhor ir para outro file
-
-const { jwt } = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 const verifyMiddleware = async (req, res, next) => {
-  jwt.verify(req.token, process.env.JWT_SECRET); // req.token está certo???????
+  //gets authorization header
+  const authHeader = req.headers['authorization'];
+  const header = authHeader && authHeader.split(' ');
 
-  let user = req.user;
+  if (header || header.lenght != 2 || header[0] != 'Bearer') {
+    next();
+    return;
+  }
 
-  next();
+  //uses jwt to verify if the token is valid
+  const token = header[1];
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (!err) req.user = user;
+    next();
+  });
 };
 
 module.exports = {
