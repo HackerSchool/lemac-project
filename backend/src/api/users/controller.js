e4de8318f2b24001d85989587356897e81360d1f
Diogo Matos <diogo.s.matos@tecnico.ulisboa.pt>
2021-08-08 21:29:00 +0100

    add update route

--- backend/src/api/users/controller.js
+++ backend/src/api/users/controller.js
@@ -10,18 +10,35 @@ module.exports = {
       const [results] = await database.execute(
         'SELECT * FROM users WHERE user_id=LAST_INSERT_ID()'
       );
+      database.end();
       return results[0];
     } catch (e) {
       return e.code;
     }
   },
+
   getUsers: async (database) => {
     try {
       const [results] = await database.execute('SELECT * FROM users');
+      database.end();
       return results;
     } catch (e) {
       console.error(e);
       return;
+    }
+  },
+
+  updateUser: async (database, userId, user) => {
+    try {
+      await database.execute(
+        'UPDATE users SET ist_id = ?, name = ?, active = ?, admin = ? WHERE user_id = ?',
+        [user.istId, user.name, user.active, user.admin, userId]
+      );
+      const [results] = await database.execute('SELECT * FROM users WHERE user_id = ?', [userId]);
+      database.end();
+      return results[0];
+    } catch (e) {
+      return e.code;
     }
   },
 };
--- backend/src/api/users/routes.js
+++ backend/src/api/users/routes.js
@@ -1,9 +1,10 @@
 const { asyncHandler } = require('../../middleware/requestHandler');
-const { addUser, getUsers } = require('./index');
+const { addUser, getUsers, updateUser } = require('./index');
 
 module.exports = {
   init: (app) => {
     app.post('/users', asyncHandler(addUser));
     app.get('/users', asyncHandler(getUsers));
+    app.put('/user/:id', asyncHandler(updateUser));
   },
 };
