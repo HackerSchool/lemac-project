fb40628edeb9465501090a686cac3411bb990eb1
Diogo Matos <diogo.s.matos@tecnico.ulisboa.pt>
2021-08-11 22:59:00 +0100

    configure frontend router for auth and 404 page

--- frontend/src/plugins/router.js
+++ frontend/src/plugins/router.js
@@ -1,20 +1,30 @@
+import { getAuthToken } from '@/api/httpClient.api';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '@/pages/Home.vue';
 import Login from '@/pages/Login.vue';
+import NotFoundPage from '@/pages/404.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home,
+    meta: { title: 'Home', noAuth: true },
   },
   {
     path: '/login',
-    name: 'Login',
+    name: 'login',
     component: Login,
+    meta: { title: 'Login' },
+  },
+  {
+    path: '*',
+    name: 'page-not-found',
+    component: NotFoundPage,
+    meta: { title: 'Page Not Found', noAuth: true },
   },
 ];
 
@@ -23,6 +33,26 @@ const router = new VueRouter({
   base: process.env.BASE_URL || '/',
   redirect: '/',
   routes,
+});
+
+router.beforeEach((to, _from, next) => {
+  // Change the document title
+  const nearestWithTitle = to.matched
+    .slice()
+    .reverse()
+    .find((r) => r.meta && r.meta.title);
+  if (nearestWithTitle) document.title = `${nearestWithTitle.meta.title} | LEMAC`;
+  else document.title = 'LEMAC';
+
+  if (to.matched.some((record) => !record.meta.noAuth) && !getAuthToken()) {
+    next({
+      path: '/',
+      params: { nextUrl: to.fullPath },
+    });
+    return;
+  }
+
+  next();
 });
 
 export default router;
