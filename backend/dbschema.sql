79009882a609cc3056e599d34f3825e9efc5451a
Diogo Matos <diogo.s.matos@tecnico.ulisboa.pt>
2021-08-25 22:12:18 +0100

    update dbschema to include notes and entries

--- backend/dbschema.sql
+++ backend/dbschema.sql
@@ -13,16 +13,35 @@ CREATE TABLE IF NOT EXISTS `log_hours` (
   `user_id` int,
   `entry` datetime,
   `exit` datetime,
-  `time` int,
-  
-  FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+  `time` int
 );
 
-CREATE TABLE `workstations` (
+CREATE TABLE IF NOT EXISTS `workstations` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255),
   `state` bool DEFAULT false,
   `type` ENUM ('active', 'disabled', 'remote')
 );
+
+CREATE TABLE IF NOT EXISTS `entries` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `workstation_id` int,
+  `ist_id` varchar(12),
+  `created_at` timestamp DEFAULT (now()),
+  `active` bool DEFAULT true
+);
+
+CREATE TABLE IF NOT EXISTS `notes` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int,
+  `created_at` timestamp DEFAULT (now()),
+  `text` text
+);
+
+ALTER TABLE `log_hours` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+ALTER TABLE `entries` ADD FOREIGN KEY (`workstation_id`) REFERENCES `workstations` (`id`);
+
+ALTER TABLE `notes` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
 COMMIT;
