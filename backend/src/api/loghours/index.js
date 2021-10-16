5a6981337c4bfcbd0f20547f386870041d68a4fb
joaoandrade8 <joao.andrade.oliveira@tecnico.ulisboa.pt>
2021-10-16 18:26:58 +0100

    loghours

--- backend/src/api/loghours/index.js
+++ backend/src/api/loghours/index.js
@@ -1,3 +1,13 @@
 const controller = require('./controller');
 
-module.exports = {};
+module.exports = {
+    verifiesUser: async (req, res) => { //verifies if the user exists and is authorized to do the operation
+        if (!req.user || !req.user.admin){
+            res.sendStatus(401);
+            return;
+        }
+    },
+    addEntryHours: async(req, res) => {
+        
+    },
+};
