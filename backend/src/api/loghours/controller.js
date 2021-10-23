bec712c9516b98aefb93cd673e05decf5a4549e6
ana <anaisabelmartins@tecnico.ulisboa.pt>
2021-10-23 12:50:16 +0100

    closes #18 finished get hours

--- backend/src/api/loghours/controller.js
+++ backend/src/api/loghours/controller.js
@@ -1,5 +1,13 @@
 module.exports = {
-    addHours: async (database, entryHours) => {
-        
+  addHours: async (database, entryHours) => {},
+  getHours: async (database) => {
+    try {
+      const [results] = await database.execute('SELECT * FROM log_hours');
+      database.end();
+      return results;
+    } catch (e) {
+      console.error(e);
+      return;
     }
+  },
 };
--- backend/src/api/loghours/index.js
+++ backend/src/api/loghours/index.js
@@ -1,27 +1,55 @@
 const controller = require('./controller');
 
 module.exports = {
-    verifiesUser: async (req, res) => { //verifies if the user exists and is authorized to do the operation
-        if (!req.user || !req.user.admin){
-            res.sendStatus(401);
-            return;
-        }
-    },
-    addHours: async(req, res) => {
-        if (req.body){
-            const data = await controller.addHours(req.db, req.body);
-            if (data == 'ER_DUP_ENTRY'){
-                res.status(409).send('duplicate entry');
-                return;
-            }
-            const response = {
-                entryH: data.entry_hours,
-                exitH: data.exist_hours,
-                time: data.time,
-            };
-            res.json(response);
-            return;
-        }
-        res.sendStatus(400);
-    },
+  verifiesUser: async (req, res) => {
+    //verifies if the user exists and is authorized to do the operation
+    if (!req.user || !req.user.admin) {
+      res.sendStatus(401);
+      return;
+    }
+  },
+  addHours: async (req, res) => {
+    if (req.body) {
+      const data = await controller.addHours(req.db, req.body);
+      if (data == 'ER_DUP_ENTRY') {
+        res.status(409).send('duplicate entry');
+        return;
+      }
+      const response = {
+        entryH: data.entry_hours,
+        exitH: data.exist_hours,
+        time: data.time,
+      };
+      res.json(response);
+      return;
+    }
+    res.sendStatus(400);
+  },
+  getHours: async (req, res) => {
+    //auth check
+    if (!req.user || !req.user.admin) {
+      res.sendStatus(401);
+      return;
+    }
+
+    const data = await controller.getHours(req.db);
+    if (data.length === 0) {
+      //no hours in db
+      res.json([]);
+      return;
+    } else if (data.length > 0) {
+      const response = data.map((x) => ({
+        id: x.user_id,
+        userId: x.user_id,
+        entry: x.entry,
+        exit: x.exit,
+        time: x.time,
+      }));
+      res.json(response);
+      return;
+    } else {
+      //bad request
+      res.sendStatus(400);
+    }
+  },
 };
