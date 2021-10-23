6f4cab2a153ca299809f6c4b89e1d49a71ee9784
Diogo Matos <diogo.s.matos@tecnico.ulisboa.pt>
2021-10-23 23:34:09 +0100

    finish post request log hours closes #15

--- backend/src/api/loghours/controller.js
+++ backend/src/api/loghours/controller.js
@@ -1,5 +1,19 @@
 module.exports = {
-  addHours: async (database, entryHours) => {},
+  addHours: async (database, hours, userId) => {
+    try {
+      const time = 0;
+      await database.execute(
+        'INSERT INTO `log_hours` (user_id, entry, `exit`, time) VALUES ( ? , ? , ? , ? )',
+        [userId, hours.entry, hours.exit, time]
+      );
+      const [results] = await database.execute('SELECT * FROM log_hours WHERE id=LAST_INSERT_ID()');
+      database.end();
+      return results[0];
+    } catch (e) {
+      console.error(e);
+      return;
+    }
+  },
   getHours: async (database) => {
     try {
       const [results] = await database.execute('SELECT * FROM log_hours');
