f92eef2bf4044424f70ac59bf4d0021af5b2aff8
ana <anaisabelmartins@tecnico.ulisboa.pt>
2021-10-23 12:50:16 +0100

    closes #18 finished get hours

--- backend/src/api/loghours/routes.js
+++ backend/src/api/loghours/routes.js
@@ -1,9 +1,10 @@
 const { asyncHandler } = require('../../middleware/requestHandler');
-const {verifiesUser, addHours} = require('./index');
+const { verifiesUser, addHours, getHours } = require('./index');
 
 module.exports = {
   init: (app) => {
     app.post('/loghours', asyncHandler(verifiesUser));
     app.post('/loghours', asyncHandler(addHours));
+    app.get('/loghours', asyncHandler(getHours));
   },
 };
