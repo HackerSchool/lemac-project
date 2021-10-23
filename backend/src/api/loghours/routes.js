3e21faf2a23ebb82fe0d2def0c80a4cbc011cd75
Diogo Matos <diogo.s.matos@tecnico.ulisboa.pt>
2021-10-23 23:34:09 +0100

    finish post request log hours closes #15

--- backend/src/api/loghours/routes.js
+++ backend/src/api/loghours/routes.js
@@ -1,9 +1,8 @@
 const { asyncHandler } = require('../../middleware/requestHandler');
-const { verifiesUser, addHours, getHours } = require('./index');
+const { addHours, getHours } = require('./index');
 
 module.exports = {
   init: (app) => {
-    app.post('/loghours', asyncHandler(verifiesUser));
     app.post('/loghours', asyncHandler(addHours));
     app.get('/loghours', asyncHandler(getHours));
   },
