2905a7b9232cea51fd0e93fdbc1816520999b0c1
Diogo Matos <diogo.s.matos@tecnico.ulisboa.pt>
2021-10-24 10:47:57 +0100

    bug fixes and small changes

--- backend/src/api/loghours/routes.js
+++ backend/src/api/loghours/routes.js
@@ -1,11 +1,11 @@
 const { asyncHandler } = require('../../middleware/requestHandler');
-const { verifiesUser, addHours, getHours, getIndividualHours, deleteHours } = require('./index');
+const { addHours, getHours, getIndividualHours, deleteHours } = require('./index');
 
 module.exports = {
   init: (app) => {
     app.post('/loghours', asyncHandler(addHours));
     app.get('/loghours', asyncHandler(getHours));
-    app.get('/loghours/:id', asyncHandler(getIndividualHours));
+    app.get('/loghours/self', asyncHandler(getIndividualHours));
     app.delete('/loghours/:id', asyncHandler(deleteHours));
   },
 };
