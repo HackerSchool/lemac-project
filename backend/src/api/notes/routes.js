d100900e02d9fc4bde921f9e6117b0f61975c179
joaoandrade8 <joao.andrade.oliveira@tecnico.ulisboa.pt>
2021-12-05 18:23:10 +0000

    get and delete notes

--- backend/src/api/notes/routes.js
+++ backend/src/api/notes/routes.js
@@ -1,9 +1,11 @@
 const { asyncHandler } = require('../../middleware/requestHandler');
-const { addNotes, updateNotes } = require('./index');
+const { addNotes, updateNotes, getNotes, deleteNote } = require('./index');
 
 module.exports = {
   init: (app) => {
     app.post('/notes', asyncHandler(addNotes));
     app.put('/notes/:id', asyncHandler(updateNotes));
+    app.get('/notes', asyncHandler(getNotes));
+    app.delete('/notes', asyncHandler(deleteNote));
   },
 };
