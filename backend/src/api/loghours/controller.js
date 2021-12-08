cd7ef8bdf198180a10315c654f5a23d343bcb385
Diogo Matos <diogo.s.matos@tecnico.ulisboa.pt>
2021-12-08 16:53:41 +0000

    get sum hours backend

--- backend/src/api/loghours/controller.js
+++ backend/src/api/loghours/controller.js
@@ -74,7 +74,19 @@ module.exports = {
       //only user can delete its own hours
       if (results.length === 0 || userId !== results[0].user_id) return false;
       await database.execute('DELETE FROM log_hours WHERE id = ?', [id]);
+      database.end();
       return true;
+    } catch (e) {
+      console.error(e);
+    }
+  },
+  getSum: async (database) => {
+    try {
+      const [results] = await database.execute(
+        'SELECT l.user_id, SUM(l.time) as time, u.name FROM log_hours l LEFT JOIN users u USING (user_id) GROUP by user_id'
+      );
+      database.end();
+      return results;
     } catch (e) {
       console.error(e);
     }
