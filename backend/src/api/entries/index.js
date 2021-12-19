6ff3839e8d4f97228756fbb8e34e1893b8c49c12
ana <anaisabelmartins@tecnico.ulisboa.pt>
2021-12-19 11:55:49 +0000

    hehe

--- backend/src/api/entries/index.js
+++ backend/src/api/entries/index.js
@@ -1,5 +1,17 @@
+const { asyncHandler } = require('backend/src/middleware/requestHandler');
 const controller = require('./controller');
 
 module.exports = {
-    addEntries: 
-}
+  addEntries: async (req) => {
+    //condição de erro aqui
+    //condição
+    const data = await controller.addEntries(req.db, req.body, req.workstations.id);
+
+    const response = {
+      workstationId: data.workstation_id,
+      istId: data.ist_id,
+    };
+    response.json(response);
+    return;
+  },
+};
