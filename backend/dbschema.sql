4dfefab61c263c39d5f493ea20bb21852e4dc357
Diogo Matos <diogo.s.matos@tecnico.ulisboa.pt>
2021-12-23 19:22:04 +0000

    update db schema

--- backend/dbschema.sql
+++ backend/dbschema.sql
@@ -19,7 +19,8 @@ CREATE TABLE IF NOT EXISTS `log_hours` (
 CREATE TABLE IF NOT EXISTS `workstations` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255),
-  `state` bool DEFAULT false,
+  `capacity` int DEFAULT 0,
+  `occupation` int DEFAULT 0,
   `type` ENUM ('active', 'disabled', 'remote')
 );
 
@@ -28,7 +29,8 @@ CREATE TABLE IF NOT EXISTS `entries` (
   `workstation_id` int,
   `ist_id` varchar(12),
   `created_at` timestamp DEFAULT (now()),
-  `active` bool DEFAULT true
+  `active` bool DEFAULT true,
+  `observations` text
 );
 
 CREATE TABLE IF NOT EXISTS `notes` (
@@ -38,10 +40,10 @@ CREATE TABLE IF NOT EXISTS `notes` (
   `text` text
 );
 
-ALTER TABLE `log_hours` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+ALTER TABLE `log_hours` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE SET NULL;
 
-ALTER TABLE `entries` ADD FOREIGN KEY (`workstation_id`) REFERENCES `workstations` (`id`);
+ALTER TABLE `entries` ADD FOREIGN KEY (`workstation_id`) REFERENCES `workstations` (`id`) ON DELETE SET NULL;
 
-ALTER TABLE `notes` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+ALTER TABLE `notes` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE SET NULL;
 
 COMMIT;
