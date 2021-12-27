2a1de61e17198ea36705f039a24891e21f63bd38
ana <anaisabelmartins@tecnico.ulisboa.pt>
2021-12-27 12:25:41 +0000

    finished addEntries

--- backend/src/api/entries/controller.js
+++ backend/src/api/entries/controller.js
@@ -1,12 +1,11 @@
 module.exports = {
-  addEntries: async (database, entries, istId, workstationId) => {
+  addEntries: async (database, istId, workstationId) => {
     try {
       await database.execute('INSERT INTO `entries` (ist_id, workstation_id) VALUES ( ? , ? )', [
         istId,
-        workstationId, //como isto vem de outra tabela é assim né?? e não é preciso trazer o entries??
+        workstationId,
       ]);
       const [results] = await database.execute('SELECT * FROM `entries` WHERE id=LAST_INSERT_ID()');
-      database.end();
       return results[0];
     } catch (e) {
       console.error(e);
