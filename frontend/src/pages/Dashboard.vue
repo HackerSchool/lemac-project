c49aa78a853e399c3d228bf4f2f5438cf8a70d72
Diogo Matos <diogo.s.matos@tecnico.ulisboa.pt>
2021-12-28 20:06:42 +0000

    prepare entries frontend

--- frontend/src/pages/Dashboard.vue
+++ frontend/src/pages/Dashboard.vue
@@ -1,9 +1,23 @@
 <template>
-  <div>Dashboard placeholder</div>
+  <v-tabs grow>
+    <v-tab key="1">Registration</v-tab>
+    <v-tab key="2">Occurences</v-tab>
+    <v-tab-item>
+      <v-container>
+        <EntriesList />
+      </v-container>
+    </v-tab-item>
+    <v-tab-item>
+      <v-container> </v-container>
+    </v-tab-item>
+  </v-tabs>
 </template>
 
 <script>
-export default {};
+import EntriesList from '@/components/Entries/EntriesList.vue';
+export default {
+  components: { EntriesList },
+};
 </script>
 
 <style></style>
