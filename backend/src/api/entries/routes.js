735dc43ff2a0e2d05ebfe456ffd9b3797a0b727a
ana <anaisabelmartins@tecnico.ulisboa.pt>
2021-12-28 12:09:16 +0000

    finished getEntries

--- backend/src/api/entries/routes.js
+++ backend/src/api/entries/routes.js
@@ -1,8 +1,9 @@
 const { asyncHandler } = require('../../middleware/requestHandler');
-const { addEntries } = require('./index');
+const { addEntries, getEntries } = require('./index');
 
 module.exports = {
   init: (app) => {
     app.post('/entries', asyncHandler(addEntries));
+    app.get('/entries', asyncHandler(getEntries));
   },
 };
