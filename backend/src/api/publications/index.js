2b7a4751cac8a578d5e4c6d71471c272853a1cbd
ana <anaisabelmartins@tecnico.ulisboa.pt>
2021-12-29 12:33:54 +0000

    add and delete

--- backend/src/api/publications/index.js
+++ backend/src/api/publications/index.js
@@ -1,3 +1,44 @@
 const controller = require('./controller');
 
-module.exports = {};
+module.exports = {
+  addPublication: async (req, res) => {
+    if (!req.user || !req.user.admin) {
+      res.sendStatus(401);
+      return;
+    }
+    if (req.body && req.body.title && req.body.text) {
+      const data = await controller.addPublication(req.db, req.body.title, req.body.text);
+
+      const response = {
+        id: data.id,
+        title: data.title,
+        text: data.text,
+        createdAt: data.created_at,
+        active: data.active,
+      };
+      res.json(response);
+      return;
+    } else {
+      res.sendStatus(400);
+      return;
+    }
+  },
+  deletePublication: async (req, res) => {
+    if (!req.user || !req.user.admin) {
+      res.sendStatus(401);
+      return;
+    }
+    try {
+      if (await controller.deletePublication(req.db, req.params.id)) {
+        res.sendStatus(204); // no content - success
+        return;
+      } else {
+        res.sendStatus(404); // not found
+        return;
+      }
+    } catch (e) {
+      res.sendStatus(400);
+      return;
+    }
+  },
+};
