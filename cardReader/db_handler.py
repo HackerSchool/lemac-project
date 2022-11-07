887a8a2e8f15da7a789e5afcafbfe03434587fc3
Francisco Carvalho <franciscojcarvalho@tecnico.ulisboa.pt>
2022-11-07 21:11:51 +0000

    implemented `db_handler.execute_query`

--- cardReader/db_handler.py
+++ cardReader/db_handler.py
@@ -2,13 +2,46 @@
 This file handles communication with the databse
 """
 
+import database_connection_exception
+import requests
+import configparser
+import mysql.connector
+
+
+
+
 class db_handler:
     def __init__(self) -> None:
-        pass
+        config = configparser()
+        config.read("config.ini")
 
-    def id_lookup(self, id:str) -> int:
-        """
-        returns: istID associated with it
-                 -1 if not in the database
-        """ 
-        pass
+        self.WEBSITE_URL = config.get("website","WEBSITE_URL")
+        self.database_credentials = {
+            "host":config.get("database","DATABASE_URL"),
+            "user":config.get("database","DATABASE_USER"),
+            "password":config.get("database","DATABASE_PASSWD"),
+            "database":"",
+        }
+
+    def execute_query(self, query:str, params:tuple) -> str:
+        try:
+            db_con = mysql.connector.connect(**self.database_credentials)
+            cursor = db_con.cursor()
+
+            cursor.execute(query, params)
+
+            return cursor
+            
+        except mysql.connector.Error as e:
+            raise database_connection_exception(e)
+        finally:
+            db_con.close()
+    
+    def send_msg_to_endpoint(self, endpoint:str, params:dict) -> None:
+        returnValue = requests.get(
+            self.WEBSITE_URL+"/"+endpoint, 
+            params
+        )
+        
+        if not returnValue.ok:
+            raise database_connection_exception()
--- cardReader/reader.py
+++ cardReader/reader.py
@@ -8,19 +8,41 @@ from mfrc522 import SimpleMFRC522
 import db_handler
 import database_connection_exception
 
+
 reader = SimpleMFRC522()
 
 database = db_handler()
 
+
+def id_lookup(id:int) -> int:
+        """
+        returns: istID associated with it
+                 -1 if not in the database
+        """ 
+        pass
+
+def send_card_information(ist_id:int):
+    database.send_msg_to_endpoint(
+        "cardEndpoint", {"ist_id":ist_id}
+    )
+
 try:
-    id,text = reader.read()
+    mifare_id,text = reader.read()
     print(id)
     try:
-        if (ist_id := database.id_lookup(id)) is not None:
-            print("Recognized", ist_id)
+        # Ceck if there is an ist_id associated with this mifare_id
+        if (card_ist_id := id_lookup(mifare_id)) is not None:
+            print("Recognized", card_ist_id)
+            send_card_information(card_ist_id)
+        # Association not found
+        else:
+            print("New card detected with MIFARE ID", id)
+            send_card_information(-1)
+
     except database_connection_exception as e:
         print("There was an error connecting to the database")
+        
+        # TODO: ERR LED FLASHING
 finally:
     GPIO.cleanup()
-
 
